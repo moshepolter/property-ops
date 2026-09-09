2e62c3a08bd929ebfd3aa5439236a77d98c0e9ca
moshepolter <44886334+moshepolter@users.noreply.github.com>
2026-09-08 21:32:08 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: "esnext",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext",
+    },
+  },
 });
